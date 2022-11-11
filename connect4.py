97198d4d2cbd7797d53ffb493296df159a8d970e
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-11-11 21:34:32 +0200

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -65,27 +65,58 @@ def winning_game(board, piece):
             if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
                 return True
 
+def evaluate_window(window, piece):
+	score = 0
+	opp_piece = PLAYER_PIECE
+	if piece == PLAYER_PIECE:
+		opp_piece = AI_PIECE
+
+	if window.count(piece) == 4:
+		score += 100
+	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+		score += 5
+	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+		score += 2
+
+	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+		score -= 4
+
+	return score
+
 def heuristic(board, piece):
-    score=0
-    #check horizontal
-    for r in range(ROW):
-        row_array = [int(i) for i in list(board[r,:])]
-        for c in range(COLUMN-3):
-            window = row_array[c:c+4]
-            if window.count(piece) == 4:
-                score+=100
-            elif window.count(piece) == 3 and window.count(empty) == 1:
-                score+=10
-    return score
-    #CHECK VERTICAL
-    for c in range(COLUMN):
-        col_array = [int(i) for i in list(board[:,c])]
-        for r in range (row-2):
-            window = col_array[r:r+4]
-            if window.count(piece) == 4:
-                score+=100
-            elif window.count(piece) == 3 and window.count(empty) == 1:
-                score+=10
+	score = 0
+
+	## Score center column
+	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+	center_count = center_array.count(piece)
+	score += center_count * 3
+
+	## Score Horizontal
+	for r in range(ROW_COUNT):
+		row_array = [int(i) for i in list(board[r,:])]
+		for c in range(COLUMN_COUNT-3):
+			window = row_array[c:c+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score Vertical
+	for c in range(COLUMN_COUNT):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(ROW_COUNT-3):
+			window = col_array[r:r+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score posiive sloped diagonal
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	return score
 
 def get_valid_locations(board):
     valid_locations = []
