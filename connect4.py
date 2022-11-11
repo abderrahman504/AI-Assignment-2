17d2ed7ce5940bc9326c4dfb6c37ef9195bb5b40
Nada Ali <96360993+nada-ali1711@users.noreply.github.com>
2022-11-11 21:40:55 +0200

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -73,12 +73,12 @@ def evaluate_window(window, piece):
 
 	if window.count(piece) == 4:
 		score += 100
-	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+	elif window.count(piece) == 3 and window.count(empty) == 1:
 		score += 5
-	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+	elif window.count(piece) == 2 and window.count(empty) == 2:
 		score += 2
 
-	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+	if window.count(opp_piece) == 3 and window.count(empty) == 1:
 		score -= 4
 
 	return score
@@ -87,33 +87,33 @@ def heuristic(board, piece):
 	score = 0
 
 	## Score center column
-	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+	center_array = [int(i) for i in list(board[:, COLUMN//2])]
 	center_count = center_array.count(piece)
 	score += center_count * 3
 
 	## Score Horizontal
-	for r in range(ROW_COUNT):
+	for r in range(ROW):
 		row_array = [int(i) for i in list(board[r,:])]
-		for c in range(COLUMN_COUNT-3):
-			window = row_array[c:c+WINDOW_LENGTH]
+		for c in range(COLUMN-3):
+			window = row_array[c:c+4]
 			score += evaluate_window(window, piece)
 
 	## Score Vertical
-	for c in range(COLUMN_COUNT):
+	for c in range(COLUMN):
 		col_array = [int(i) for i in list(board[:,c])]
 		for r in range(ROW_COUNT-3):
-			window = col_array[r:r+WINDOW_LENGTH]
+			window = col_array[r:r+4]
 			score += evaluate_window(window, piece)
 
 	## Score posiive sloped diagonal
-	for r in range(ROW_COUNT-3):
-		for c in range(COLUMN_COUNT-3):
-			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+	for r in range(ROW-3):
+		for c in range(COLUMN-3):
+			window = [board[r+i][c+i] for i in range(4)]
 			score += evaluate_window(window, piece)
 
-	for r in range(ROW_COUNT-3):
-		for c in range(COLUMN_COUNT-3):
-			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+	for r in range(ROW-3):
+		for c in range(COLUMN-3):
+			window = [board[r+3-i][c+i] for i in range(4)]
 			score += evaluate_window(window, piece)
 
 	return score
@@ -138,8 +138,3 @@ def best_move(board, piece):
             best_score = score
             best_col = col
     return best_col
-
-
-
-
-
